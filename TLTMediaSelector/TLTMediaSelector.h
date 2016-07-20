a713329b9fffdf964af20936f5186c5f4ea1318d
Martin Rehder <apple01@rehsco.com>
2016-07-20 12:16:47 +0200

    Initial Commit

---
+++ TLTMediaSelector/TLTMediaSelector.h
@@ -0,0 +1,19 @@
+//
+//  TLTMediaSelector.h
+//  TLTMediaSelector
+//
+//  Created by Martin Rehder on 20.07.16.
+//  Copyright © 2016 Martin Rehder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TLTMediaSelector.
+FOUNDATION_EXPORT double TLTMediaSelectorVersionNumber;
+
+//! Project version string for TLTMediaSelector.
+FOUNDATION_EXPORT const unsigned char TLTMediaSelectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TLTMediaSelector/PublicHeader.h>
+
+
